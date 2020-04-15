9586514c8817e6434a2b5a9fe3d73653ffd15e19
Jamie Polackwich <rjpolackwich@gmail.com>
2020-04-15 10:33:02 -0600

    moving fns to utils

--- skyway/utils.py
+++ skyway/utils.py
@@ -1,6 +1,31 @@
 from rasterio.windows import Window
+import shapely.geometry as geom
 
 with rasterio.open(impath) as src:
     plt.figure(figsize=(10, 10))
     arr = src.read(window=Window(512, 512, 512*2, 512*2))
     rashow(arr)
+
+
+def itrreduce(fn, iterable, initializer=None):
+    it = iter(iterable)
+    if initializer is None:
+        value = next(it)
+    else:
+        value = initializer
+    for element in it:
+        value = fn([value, element])
+    return value
+
+
+def to_gjson(tile):
+    geodata = geom.mapping(t)
+    gj = {"type": "Feature",
+         "properties": {"tile_id": "+".join([str(t.utm_zone), t.quadkey]),
+                        "ix": t.xi,
+                        "iy": t.yi,
+                        "zoom": t.zoom},
+         "geometry": geodata}
+    return gj
+
+
