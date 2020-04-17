296b0d834c252f90b8d8a9b985b89aa8839d0211
Jamie Polackwich <rjpolackwich@gmail.com>
2020-04-17 14:45:55 -0600

    creating query type params

--- skyway/base.py
+++ skyway/base.py
@@ -3,28 +3,129 @@ import dataclasses
 import json
 import csv
 import datetime
+from dateutil.parser import parse as dateparse
 import requests
 
 
+class QueryBbox():
+    _qname = "bbox"
+
+    def __init__(self, bbox: typing.typing.Sequence):
+        self.bbox = bbox
+
+    def __repr__(self):
+        enc = ",".join(map(str, self.bbox))
+        return f'''[{self._qname}:{enc}]'''
+
+    @property
+    def bbox(self):
+        return [self.south,
+                self.west,
+                self.north,
+                self.east
+                ]
+
+    @bbox.setter
+    def bbox(self, bbox):
+        s, w, n, e = bbox
+        self.south = s
+        self.west = w
+        self.north = n
+        self.east = e
+
+class QueryTimeout():
+    _qname = "timeout"
+
+    def __init__(self, timeout=360):
+        self.timeout = timeout
+
+    def __repr__(self):
+        return f'''[{self._qname}:{self.timeout}]'''
+
+
+
+class QueryPayloadFormat():
+    _qname = "out"
+
+
+class FormatNotImplemented(QueryPayloadFormat):
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class JSONPayload(QueryPayloadFormat):
+    def __repr__(self):
+        return f'''[{self._qname}:json]'''
+
+
+class CSVPayload(QueryPayloadFormat):
+    def __init__(self, fields=[]):
+        self.fields = fields
+
+    def __repr__(self):
+        return f'''[{self._qname}:csv()]''' # fix
+
+
+class XMLPayload(FormatNotImplemented): ...
+class CustomPayload(FormatNotImplemented): ...
+class PopupPayload(FormatNotImplemented): ...
+
+
+class QueryDate():
+    _qname = "date"
+
+    def __init__(self, date: typing.typing.Union[str, datetime.datetime] = None):
+        if isinstance(date, str):
+            date = dateparse(date)
+        assert isinstance(date, datetime.datetime)
+        self.date = date
+
+    def __repr__(self):
+        return f'''[{self._qname}:"{self.date.isoformat()}"]'''
+
+
+class DateDelta(QueryDate):
+    _qname = "diff"
+
+class AugmentedDateDelta(DateDelta):
+    _qname = "adiff"
+
+
+
+
+
+
 @dataclasses.dataclass
-class BaseQuerySettings:
-    out: str
+class QuerySettings:
+    payload_format: str
     maxsize: int
     timeout: int
-    date: typing.typing.Union[str, datetime.datetime] = None
-    bbox: tuple = None
+    datespec: typing.typing.Union[str, datetime.datetime]
+    bbox: typing.typing.Sequence = dataclasses.field(default_factory=list)
+
+
+    def __post_init__(self):
+        pass
 
     def __repr__(self):
         msg = list()
         for k, v in dataclasses.asdict(self).items():
             if v is not None:
-                msg.append(f'''[{k}:{v}]''')
+                msg.append(f'''{v}''')
         return "".join(msg) + ";"
 
+    @classmethod
+    def geom_settings(cls, date=None, bbox=None):
+        return cls("json", 1073741824, 360, date, bbox)
 
-class OSMElement():
-    pass
+    @classmethod
+    def stats_settings(cls, date=None, bbox=None):
+        return cls("csv", 536870912, 360, date, bbox)
 
+
+
+
+class OSMElement(): pass
 
 class Node(OSMElement):
     _type = "node"
@@ -36,12 +137,6 @@ class Way(OSMElement):
 
 class Relation(OSMElement):
     _type = "relation"
-
-
-@dataclasses.dataclass
-class BaseQuery:
-    elements: typing.typing.List[OSMElement]
-    tags: str
 
 
 
--- skyway/utils.py
+++ skyway/utils.py
@@ -1,10 +1,12 @@
 from rasterio.windows import Window
 import shapely.geometry as geom
+import rasterio
 
-with rasterio.open(impath) as src:
-    plt.figure(figsize=(10, 10))
-    arr = src.read(window=Window(512, 512, 512*2, 512*2))
-    rashow(arr)
+def balh(impath):
+    with rasterio.open(impath) as src:
+        plt.figure(figsize=(10, 10))
+        arr = src.read(window=Window(512, 512, 512*2, 512*2))
+        rashow(arr)
 
 
 def itrreduce(fn, iterable, initializer=None):
@@ -19,12 +21,12 @@ def itrreduce(fn, iterable, initializer=None):
 
 
 def to_gjson(tile):
-    geodata = geom.mapping(t)
+    geodata = geom.mapping(tile)
     gj = {"type": "Feature",
-         "properties": {"tile_id": "+".join([str(t.utm_zone), t.quadkey]),
-                        "ix": t.xi,
-                        "iy": t.yi,
-                        "zoom": t.zoom},
+         "properties": {"tile_id": "+".join([str(tile.utm_zone), tile.quadkey]),
+                        "ix": tile.xi,
+                        "iy": tile.yi,
+                        "zoom": tile.zoom},
          "geometry": geodata}
     return gj
 
